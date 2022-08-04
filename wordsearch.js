65bf49b0d571f5e6e7120aefb650ef320593fff5
John Sweeney <jes2806@gmail.com>
2022-08-03 18:42:35 -0600

    word search paired programming

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,20 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  let string = "";
+  let verticalWords = [];
+  const verticalJoin = letters[0].map((_, i) => letters.map(r => string += r[i]) && verticalWords.push(string) && (string = ""));
 
-module.exports = wordSearch
+  for (l of horizontalJoin) {
+    if (l.includes(word)) return true
+  };
+
+  for (i of verticalWords) {
+    if (i.includes(word))
+      return true;
+  };
+  return false;
+};
+
+module.exports = wordSearch; // pair programming with Jason Li
+
+// nested map methods was how mentor walked us through this problem.  Still very hazy.  I can semi see how it would be done in a c style for loop
